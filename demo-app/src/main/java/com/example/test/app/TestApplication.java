d94d441b3a0f167408983a40c1a967c645be351a
ACh Sulfate <dex@libart.so>
2024-10-29 21:11:46 +0800

    chore: add tests

--- demo-app/src/main/java/com/example/test/app/TestApplication.java
+++ demo-app/src/main/java/com/example/test/app/TestApplication.java
@@ -2,20 +2,11 @@ package com.example.test.app;
 
 import android.app.Application;
 
-import dev.tmpfs.libcoresyscall.core.Syscall;
-import dev.tmpfs.libcoresyscall.core.impl.ReflectHelper;
-
 public class TestApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // initialize your application here
-        try {
-            Class.forName(Syscall.class.getName(), true, Syscall.class.getClassLoader());
-        } catch (ClassNotFoundException e) {
-            throw ReflectHelper.unsafeThrow(e);
-        }
     }
 
 }
